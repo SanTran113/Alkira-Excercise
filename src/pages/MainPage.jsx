150208eb8efe77fcddcfdab56c24e7fb7564a45e
SanTran113 <stran113@calpoly.edu>
2026-08-12 16:35:00 -0700

    feat: logout

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../components/AuthContext.jsx";
+
 function MainPage() {
+    const navigate = useNavigate();
+    const { logout } = useAuth();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/");
+    }
+
     return (
         <div>
             Main Page
+            <button onClick = {() => handleLogout()}>logout</button>
         </div>
     )
 }
